26a37e7d2b230b774a8107b3a168c855fda9c54f
Nadeem Anvaritafti <nanvari@live.com>
2019-11-07 14:10:51 -0600

    created skeleton for app

---
+++ redux-thunk/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NBAList from './components/NBAList';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NBAList />
+    </div>
+  );
+}
+
+export default App;
---
+++ redux-thunk/src/reducers/index.js
@@ -0,0 +1,14 @@
+
+
+const initialState = {
+    nbaData: [],
+    isFetching: false,
+    error: ''
+};
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
